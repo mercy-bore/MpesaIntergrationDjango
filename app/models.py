b4426ec771fdd86c1f376d85539012377037dbfb
Mercy Bore <mercycherotich757@gmail.com>
2022-05-30 13:35:31 +0300

    create a user model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class User(models.Model):
+    '''
+    This is a user model that has all the information about the user
+    '''
+    first_name =  models.CharField(max_length = 150)
+    last_name = models.CharField(max_length = 150)
+    password = models.CharField(max_length = 30)
+    phone_number  = models.IntegerField(max_length = 12)
+    email = models.EmailField(max_length = 30)
