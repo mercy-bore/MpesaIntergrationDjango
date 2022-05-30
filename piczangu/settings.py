4e70aab8a30638a120788cda5576e494c2fe7bd8
Mercy Bore <mercycherotich757@gmail.com>
2022-05-30 12:56:40 +0300

    create a superuser

--- piczangu/settings.py
+++ piczangu/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'piczangu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'piczangu',
+        'USER': 'mercy',
+    'PASSWORD':'cherotich1998',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
