c45b93e33fb3f45d1853780d87bdadf7bd59080d
Mercy Bore <mercycherotich757@gmail.com>
2022-05-31 11:34:53 +0300

    register event in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User,Photographer
+from .models import User,Photographer,Event
 
 # Register your models here
 
 admin.site.register(User)
 admin.site.register(Photographer)
+admin.site.register(Event)
