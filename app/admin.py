cf7b2ebe2c47bb2917737e5b0ced3f8b2f0dd0f3
Mercy Bore <mercycherotich757@gmail.com>
2022-05-31 15:21:22 +0300

    register new models on admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User,Photographer,Event,Rating
+from .models import User,Photographer,Event,Rating, PhotographerAccount,BoughtPhotos,Portfolio
 
 # Register your models here
 
@@ -7,3 +7,6 @@ admin.site.register(User)
 admin.site.register(Photographer)
 admin.site.register(Event)
 admin.site.register(Rating)
+admin.site.register(PhotographerAccount)
+admin.site.register(BoughtPhotos)
+admin.site.register(Portfolio)
