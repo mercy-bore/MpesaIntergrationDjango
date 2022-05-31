1996222b3606ab107f0947ed75d80209733a3728
Mercy Bore <mercycherotich757@gmail.com>
2022-05-31 11:34:02 +0300

    register event in admin.py

--- app/serializer.py
+++ app/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Photographer,User
+from .models import Photographer,User,
 
 class PhotographerSerializer(serializers.ModelSerializer):
     class Meta:
