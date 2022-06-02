c9fd06f59c1ce148402bdaa2592c5a2b800f7bb6
Mercy Bore <mercycherotich757@gmail.com>
2022-06-02 17:58:54 +0300

    create APIViews for events and photos

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import User,Photographer
-from .serializer import PhotographerSerializer,UserSerializer
+from .serializer import PhotographerSerializer,UserSerializer, PhotosSerializer,EventSerializer
 from rest_framework import status
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework import viewsets
@@ -25,6 +25,34 @@ class Users(APIView):
     def get(self, request,format=None):
         users = User.objects.all()
         serializers = UserSerializer(users, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = UserSerializer(post,data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Events(APIView):
+    def get(self, request,format=None):
+        events = Event.objects.all()
+        serializers = EventSerializer(events, many=True)
+        return Response(serializers.data)
+    def post(self, request, format=None):
+        serializers = UserSerializer(post,data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    
+class Photos(APIView):
+    def get(self, request,format=None):
+        photos = Photos.objects.all()
+        serializers = PhotosSerializer(photos, many=True)
         return Response(serializers.data)
     def post(self, request, format=None):
         serializers = UserSerializer(post,data=request.data)
